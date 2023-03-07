627ee2b65c15bed951f59bc2d34b3cfcc52b1664
komanosuke <toshihiko.komai616@gmail.com>
2023-03-07 14:40:40 +0900

    modified header

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_06_091533) do
+ActiveRecord::Schema.define(version: 2023_03_07_010846) do
 
   create_table "admins", force: :cascade do |t|
     t.string "name"
